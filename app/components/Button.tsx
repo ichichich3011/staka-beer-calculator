a5a84959b75f394371c4b25f864c6c4141a7a9cf
markusmaisch <markus.maisch@coliquio.de>
2023-07-13 09:48:36 +0200

    mobile second

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -9,7 +9,7 @@ const Button = ({
                     disabled,
                 }: { key?: string, onClick: () => void, children: React.ReactNode, className?: string, disabled?: boolean | undefined    }) => {
     return (
-        <button className={`p-4 text-5xl ${className} ${disabled ? 'opacity-20' : ''}`} onClick={onClick} key={key} disabled={disabled}>
+        <button className={`p-4 text-3xl md:text-5xl ${className} ${disabled ? 'opacity-20' : ''}`} onClick={onClick} key={key} disabled={disabled}>
             {children}
         </button>
     )
